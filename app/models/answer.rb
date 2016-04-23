dd758490e5e579e6c71d8954252117f2a922d14c
Afaan Naqvi <email": null>
2016-04-23 11:40:35 -0700

    Clean the skeleton

--- app/models/answer.rb
+++
@@ -1,7 +0,0 @@
-class Answer < ActiveRecord::Base
-  # Remember to create a migration!
-  has_many :comments, as: :commentable
-  has_many :votes, as: :voteable
-  belongs_to :user
-  belongs_to :post
-end
--- app/models/vote.rb
+++
@@ -1,4 +0,0 @@
-class Vote < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :voteable, polymorphic: true
-end
